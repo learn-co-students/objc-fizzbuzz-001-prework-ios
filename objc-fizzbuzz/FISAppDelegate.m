f34672778d22352b50b6036c2136bf1cd1b16a24
dominicprescod <dominicprescod@gmail.com>
2015-11-04 13:28:38 -0500

    For Loop FizzBuzz

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -1,6 +1,11 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#define START 100
+#define LIMIT 130
+#define FIZZI 3
+#define BUZZI 5
+#define NIL 0
 
 @implementation FISAppDelegate
 
@@ -14,6 +19,16 @@
      */
     
     // do not alter
+    NSString *result = @"";
+    
+    for (NSUInteger i = START; i <= LIMIT; i++) {
+        
+        if (i % FIZZI == NIL && i % BUZZI == NIL) result = @"FizzBuzz";
+        else if (i % FIZZI == NIL) result = @"Fizz";
+        else if (i % BUZZI == NIL) result = @"Buzz";
+        else result = [NSString stringWithFormat:@"%lu", i];
+        NSLog(@"%@", result);
+    }
     return YES;  //
     ///////////////
 }
