7101fbf89803c2c162427dadd069a410e18f601c
asbhat <aditya.s.bhat@gmail.com>
2015-11-25 21:20:24 -0500

    fizzbuzz!

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -7,11 +7,30 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /*
-     
-     * Write your code here!
-     
-     */
+    NSUInteger start = 1;
+    NSUInteger limit = 100;
+    
+    NSUInteger fizzIncrement = 3;
+    NSUInteger buzzIncrement = 5;
+    
+    
+    for (NSUInteger i = start; i <= limit; i++) {
+        NSString *result = @"";
+        
+        if (i % fizzIncrement == 0) {
+            result = [result stringByAppendingString:@"Fizz"];
+        }
+        
+        if (i % buzzIncrement == 0) {
+            result = [result stringByAppendingString:@"Buzz"];
+        }
+        
+        if ([result isEqualToString:@""]) {
+            result = [result stringByAppendingFormat:@"%lu", i];
+        }
+        
+        NSLog(@"%@", result);
+    }
     
     // do not alter
     return YES;  //
