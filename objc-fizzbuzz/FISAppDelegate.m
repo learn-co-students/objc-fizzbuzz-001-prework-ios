b1359afad3407d406003584582595e0b1376142a
Dnld <steinert.donald@gmail.com>
2015-12-08 18:35:33 -0500

    completes fizz buzz

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -13,6 +13,23 @@
      
      */
     
+    for (NSUInteger i = 0; i <= 100; i++) {
+        
+        NSString *result = @"";
+        
+        if (i % 3 == 0 && i % 5 == 0) {
+            result = @"FizzBuzz";
+        } else if (i % 3 == 0) {
+            result = @"Fizz";
+        } else if (i % 5 == 0) {
+            result = @"Buzz";
+        } else {
+            result = [NSString stringWithFormat:@"%li", i];
+        }
+        
+        NSLog(@"%@", result);
+    }
+    
     // do not alter
     return YES;  //
     ///////////////
