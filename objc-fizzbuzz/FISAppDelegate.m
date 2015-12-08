1122d3dbc43993152e3ebd112c09c5984562c8ec
Angie-Huang <angelay.huang@yahoo.com>
2015-12-07 23:14:55 -0500

    Did fizzbuzz including advance questions

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -13,6 +13,52 @@
      
      */
     
+/*
+    NSUInteger start = 100;
+    NSUInteger limit = 130;
+    NSUInteger fizzIncrement = 3;
+    NSUInteger buzzIncrement = 5;
+    
+    for (NSUInteger i = start; i <= limit; i++) {
+        NSString *result = @"";
+        
+        if (i % fizzIncrement == 0 && i % buzzIncrement == 0) {
+            result = @"FizzBuzz";
+        }
+        else if (i % fizzIncrement == 0) {
+            result = @"Fizz";
+        }
+        else if (i % buzzIncrement == 0) {
+            result = @"Buzz";
+        }
+        else {
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        NSLog(@"%@", result);
+    }
+ */
+    
+    NSUInteger start = 100;
+    NSUInteger limit = 130;
+    NSUInteger fizzIncrement = 3;
+    NSUInteger buzzIncrement = 5;
+    
+    for (NSUInteger i = start; i <= limit; i++) {
+        NSString *result = @"";
+        
+        if (i % fizzIncrement == 0) {
+            result = @"Fizz";
+        }
+        if (i % buzzIncrement == 0) {
+            result = [result stringByAppendingString:@"Buzz"];
+        }
+        if ([result length] == 0) {
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        
+        NSLog(@"%@", result);
+    }
+    
     // do not alter
     return YES;  //
     ///////////////
