589ca1b3b3d67b93781e50c8aae9cba77e0f05f8
yuchiz <zhu.yuchi@gmail.com>
2015-12-16 13:07:06 -0500

    Done.

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -13,6 +13,89 @@
      
      */
     
+    for (NSUInteger i = 1; i <= 100; i++) {
+        
+    }
+    
+    for (NSUInteger i = 1; i <= 100; i++) {
+        NSLog(@"%lu", i);
+    }
+
+    for (NSUInteger i = 1; i <= 100; i++) {
+        NSString *result = @"";
+        result = [NSString stringWithFormat:@"%lu", i];
+        NSLog(@"%@", result);
+    }
+    
+    // fizz
+    
+    for (NSUInteger i = 1; i <= 100; i++) {
+        NSString *result = @"";
+        
+        if (i % 3 == 0) {
+            result = @"Fizz";
+        }
+        else {
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        NSLog(@"%@", result);
+    }
+    
+    for (NSUInteger i = 1; i <= 100; i++) {
+        NSString *result = @"";
+        
+        if (i % 3 == 0) {
+            result = @"Fizz";
+        }
+        else if (i % 5 == 0) {
+            result = @"Buzz";
+        }
+        else {
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        NSLog(@"%@", result);
+    }
+    
+    for (NSUInteger i = 1; i <= 100; i++) {
+        NSString *result = @"";
+        
+        if (i % 3 == 0 && i % 5 == 0) {
+            result = @"FizzBuzz";
+        }
+        else if (i % 3 == 0) {    // becomes an 'else if' statement
+            result = @"Fizz";
+        }
+        else if (i % 5 == 0) {
+            result = @"Buzz";
+        }
+        else {
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        NSLog(@"%@", result);
+    }
+
+    NSUInteger start = 100;
+    NSUInteger limit = 130;
+    
+    for (NSUInteger i = start; i <= limit; i++) {
+        NSString *result = @"";
+        
+        if (i % 3 == 0 && i % 5 == 0) {
+            result = @"FizzBuzz";
+        }
+        else if (i % 3 == 0) {    // becomes an 'else if' statement
+            result = @"Fizz";
+        }
+        else if (i % 5 == 0) {
+            result = @"Buzz";
+        }
+        else {
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        NSLog(@"%@", result);
+    }
+    
+    
     // do not alter
     return YES;  //
     ///////////////
