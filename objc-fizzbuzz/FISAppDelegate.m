e9e60f467c399c364ed2f721d2c79a401c5d20a8
kenmhaggerty <kenmhaggerty@gmail.com>
2016-01-04 16:00:31 -0500

    Completes tasks for "Code-Along: FizzBuzz" including "Advanced" section

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -13,6 +13,30 @@
      
      */
     
+    // Advanced
+    
+    NSUInteger start = 100;
+    NSUInteger limit = 130;
+    NSString *fizz = @"Fizz";
+    NSString *buzz = @"Buzz";
+    NSUInteger fizzIncrement = 3;
+    NSUInteger buzzIncrement = 5;
+    NSString *result;
+    
+    for (NSUInteger i = start; i <= limit; i++) {
+        result = @"";
+        if (i % fizzIncrement == 0) {
+            result = fizz;
+        }
+        if (i % buzzIncrement == 0) {
+            result = [result stringByAppendingString:buzz];
+        }
+        if (result.length == 0) {
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        NSLog(@"%@", result);
+    }
+    
     // do not alter
     return YES;  //
     ///////////////
