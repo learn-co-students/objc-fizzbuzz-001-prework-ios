c739b6ced4f6010626b61902bd41ccb01aaeacf7
kevinkong91 <kevinkong91@gmail.com>
2016-01-17 00:41:14 -0500

    fizzbuzz

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -7,11 +7,26 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /*
-     
-     * Write your code here!
-     
-     */
+    NSUInteger start = 1;
+    NSUInteger end = 100;
+    
+    for (NSUInteger i = start; i <= end; i++) {
+        NSString *result = @"";
+        
+        if (i % 3 == 0) {
+            result = @"Fizz";
+            
+            if (i % 5 == 0) {
+                result = [result stringByAppendingString:@"Buzz"];
+            }
+        } else if (i % 5 == 0) {
+            result = @"Buzz";
+        } else {
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        
+        NSLog(@"%@", result);
+    }
     
     // do not alter
     return YES;  //
