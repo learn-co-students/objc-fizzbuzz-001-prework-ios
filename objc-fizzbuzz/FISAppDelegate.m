f881c88523baac6091c4456db0643774c01df409
zelosa <wanda.yanli@gmail.com>
2016-01-18 12:02:01 -0500

    adv update

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -14,6 +14,7 @@
      }
      * Write your code here!
      */
+    /*
     NSUInteger start = 100;
     NSUInteger limit = 130;
     
@@ -33,8 +34,42 @@
             result = [NSString stringWithFormat:@"%lu!", i];
         }
         NSLog(@"%@", result);
-    }
+    }*/
     
+    
+    NSUInteger start = 1;
+    NSUInteger end = 100;
+    
+    NSUInteger fizzIncrement = 3;
+    NSUInteger buzzIncrement = 5;
+    NSString *fizz = @"Fizz";
+    NSString *buzz = @"Buzz";
+    
+    for(NSUInteger i = start; i<=end; i++)
+    {
+        BOOL isFizz = i % fizzIncrement == 0;
+        BOOL isBuzz = i % buzzIncrement == 0;
+        
+        if(isFizz && isBuzz)
+        {
+            NSLog(@"%@",[fizz stringByAppendingString:buzz]);
+        }
+        if(isFizz)
+        {
+            NSLog(@"%@",fizz);
+        }
+        if(isBuzz)
+        {
+            NSLog(@"%@",buzz);
+        }
+        if (!(isFizz || isBuzz))
+        {
+            NSLog(@"%lu",i);
+        }
+        
+     
+        
+    }
     // do not alter
     return YES;  //
     ///////////////
