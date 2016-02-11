1336c51124c52d99b3d7cad3560370e335826ea3
PaknChu <paknchu@gmail.com>
2016-02-11 13:22:29 -0500

    Done.

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -5,13 +5,26 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+  
+    NSUInteger start = 1;
+    NSUInteger limit = 100;
     
-    /*
+    for(NSUInteger i = start; i <= limit; i++){
+        NSString *result = @"";
      
-     * Write your code here!
-     
-     */
+        if( i % 3 == 0 && i % 5 == 0){
+            result = @"FizzBuss";
+        } else if( i % 3 == 0) {
+            result = @"Fizz";
+        } else if ( i % 5 == 0) {
+            result = @"Buzz";
+            
+        } else {
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        
+        NSLog(@"%@", result);
+    }
     
     // do not alter
     return YES;  //
