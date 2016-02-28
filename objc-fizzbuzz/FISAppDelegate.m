07e7dcd601a5fbd3260587ab049207db054bf9a6
JohnPH75 <johnhussain75@gmail.com>
2016-02-28 16:20:56 -0500

    eighth commit

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -13,6 +13,34 @@
      
      */
     
+//    
+    NSUInteger start = 1;
+    NSUInteger limit = 101;
+//    NSUInteger fizzIncrement = 3;
+//    NSUInteger buzzIncrement = 5;
+//    
+//    for (NSUInteger i = fizzIncrement; i <= buzzIncrement; i++) {
+//        NSString *result = @"";
+    
+    for (NSUInteger i = start; i < limit; i++) {
+        NSString *result = @"";
+        
+        if (i % 3 == 0 && i % 5 == 0) {
+            result = @"FizzBuzz";
+        }
+        else if (i % 3 == 0) {
+            result = @"Fizz";
+        }
+        else if (i % 5 == 0) {
+            result = @"Buzz";
+        }
+        else {
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        NSLog(@"%@", result);
+    }
+    
+    
     // do not alter
     return YES;  //
     ///////////////
