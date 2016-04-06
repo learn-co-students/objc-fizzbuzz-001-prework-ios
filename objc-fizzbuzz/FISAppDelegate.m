db4b9ed203b18d113bf42ec014cd2f47883c9096
dpreli <dave.preli@gmail.com>
2016-04-06 11:04:23 -0400

    Fizz Buzz code-along lab

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -7,11 +7,30 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /*
-     
-     * Write your code here!
-     
-     */
+    NSUInteger start = 1;
+    NSUInteger limit = 100;
+    NSUInteger fizzincrement = 3;
+    NSUInteger buzzincrement = 5;
+    
+    
+    
+    for (NSUInteger i = start; i < limit; i++) {
+        NSString *result = @" ";
+        if (i % fizzincrement == 0 && i % buzzincrement == 0) {
+            result = @"FizzBuzz";
+        }
+        else if (i % fizzincrement == 0) {
+            result = @"Fizz";
+        }
+        else if (i % buzzincrement == 0) {
+            result = @"Buzz";
+        }
+        else {
+            result = [NSString stringWithFormat: @ "%lu", i];
+        }
+        NSLog(@"%@", result);
+    }
+    
     
     // do not alter
     return YES;  //
