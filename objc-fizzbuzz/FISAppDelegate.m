1d33d3dce75229e54ac3312091c8b675edc1b6e8
dochonglo <ismaelbarry777@hotmail.com>
2016-04-08 20:08:04 -0400

    Lab 7: Code Along (FizzBuzz) is complete.

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -13,6 +13,31 @@
      
      */
     
+    // We created variables to eliminate magic numbers.
+    NSUInteger start = 100;
+    NSUInteger limit = 130;
+    NSUInteger fizzIncrement = 3;
+    NSUInteger buzzIncrement = 5;
+    
+    for (NSUInteger i = start; i<=limit; i++) {
+        NSString *result = @"";
+        
+        if ((i%fizzIncrement == 0) && (i%buzzIncrement == 0)) {
+            result = @"Fizz Buzz";
+        } else if (i%fizzIncrement == 0) {
+            result = @"Fizz";
+        } else if (i%buzzIncrement == 0) {
+            result = @"Buzz";
+        } else {
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        
+        NSLog(@"%@", result);
+    }
+    
+    
+    
+    
     // do not alter
     return YES;  //
     ///////////////
