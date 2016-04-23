b4b0945dc3f10162e638edc26dc465f8214c5d93
dochonglo <ismaelbarry777@hotmail.com>
2016-04-23 16:49:27 -0400

    Added lab objectives for clarification purposes.

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -4,6 +4,11 @@
 
 @implementation FISAppDelegate
 
+// Objectives:
+// 1. Declare a for loop to perform a hardcoded set of iterations.
+// 2. Use if, else if, and else statements to define various behaviors based upon the loop's counter variable.
+// 3. Move the loop's iteration logic from hardcoded values into variables instead.
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
@@ -13,6 +18,7 @@
      
      */
     
+    // Advanced:
     // We created variables to eliminate magic numbers.
     NSUInteger start = 100;
     NSUInteger limit = 130;
