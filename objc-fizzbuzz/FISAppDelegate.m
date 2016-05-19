00a56fdf8b5622c60dc4dbdef4db2e1e4085a6d6
Salmaan Rizvi <sar228@cornell.edu>
2016-05-19 17:33:12 -0400

    fizz buzz

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -13,6 +13,28 @@
      
      */
     
+    NSUInteger start = 1;
+    NSUInteger limit = 100;
+    NSUInteger fizzLimit = 3;
+    NSUInteger buzzLimit = 5;
+    NSString *result = @"";
+    
+    for (NSUInteger i = start; i <= limit; i++) {
+        if(i % fizzLimit == 0 && i % buzzLimit == 0) {
+            result = @"FizzBuzz";
+        }
+        else if(i % fizzLimit == 0) {
+            result = @"Fizz";
+        }
+        else if(i % buzzLimit == 0) {
+            result = @"Buzz";
+        }
+        else {
+        result = [NSString stringWithFormat:@"%lu", i];
+        }
+        NSLog(@"%@", result);
+    }
+    
     // do not alter
     return YES;  //
     ///////////////
