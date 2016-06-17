439fe5ae14579f8b26e8d48e2a42fcbe5e6c3b93
William Robinson <wsrobins@uvm.edu>
2016-06-17 16:29:02 -0400

    Loop and conditional practice + optimization // lab complete.

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -13,6 +13,31 @@
      
      */
     
+    NSUInteger start = 1;
+    NSUInteger limit = 130;
+    NSUInteger fizzIncrement = 3;
+    NSUInteger buzzIncrement = 5;
+    NSString *fizzString = @"Fizz";
+    NSString *buzzString = @"Buzz";
+
+    for (NSUInteger i = start; i <= limit; i++) {
+        
+        NSString *result = @"";
+        
+        if (i % fizzIncrement == 0) {
+            result = [result stringByAppendingFormat:@"%@", fizzString];
+        }
+        if (i % buzzIncrement == 0) {
+            result = [result stringByAppendingFormat:@"%@", buzzString];
+        }
+        if ([result isEqualToString:@""]) {
+            result = [result stringByAppendingFormat:@"%lu", i];
+        }
+        
+        NSLog(@"%@", result);
+        
+    }
+    
     // do not alter
     return YES;  //
     ///////////////
