d53f3070e47c189c1c7513e9f3fe5bf32f8ea188
chainsawbucktooth <chainsaw.bucktooth@gmail.com>
2016-06-28 14:32:17 -0700

    finished fizzbuzz coding lab

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -7,11 +7,38 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /*
-     
-     * Write your code here!
-     
-     */
+    for (NSUInteger i = 1; i <= 100; i++) {
+        NSLog(@"%lu", i);
+    }
+    
+    for (NSUInteger i = 1; i <=100; i++) {
+        NSString *result = @"";
+        result = [NSString stringWithFormat:@"%lu", i];
+        NSLog(@"%@", result);
+    }
+    NSUInteger start = 100;
+    NSUInteger limit = 130;
+    NSUInteger fizzIncrement = 3;
+    NSUInteger buzzIncrement = 5;
+    
+    
+    for (NSUInteger i=start; i<=limit; i++) {
+        NSString *result =@"";
+        if (i % 3 ==0 && i % 5 == 0) {
+            result =@"FizzBuzz";
+        }
+        else if (i % fizzIncrement == 0) {
+            result = @"Fizz";
+        }
+        else if (i % buzzIncrement == 0) {
+            result =@"Buzz";
+        }
+        else {
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        NSLog(@"%@", result);
+    }
+
     
     // do not alter
     return YES;  //
