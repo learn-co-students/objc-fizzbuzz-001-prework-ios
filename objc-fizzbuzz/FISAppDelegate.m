40370ba03c7252f98ebcc2b27b14e3c254140902
Forrest Zhao <fozhao92@gmail.com>
2016-06-30 19:02:32 -0700

    completed

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -12,6 +12,27 @@
      * Write your code here!
      
      */
+    NSUInteger start = 100;
+    NSUInteger limit = 130;
+    NSUInteger fizzIncrement = 3;
+    NSUInteger buzzIncrement = 5;
+    
+    for (NSUInteger i = start; i <= limit; i++) {
+        NSString *result = @"";
+        if (i % fizzIncrement == 0 && i % buzzIncrement == 0) {
+            result = @"FizzBuzz";
+        }
+        else if (i % fizzIncrement == 0) {
+            result = @"Fizz";
+        }
+        else if (i % buzzIncrement == 0){
+            result = @"Buzz";
+        }
+        else{
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        NSLog(@"%@", result);
+    }
     
     // do not alter
     return YES;  //
