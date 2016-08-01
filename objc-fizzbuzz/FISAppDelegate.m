b26affc65687892f54d9d01ca512d1dc37428eeb
Henry Ly <henryly@gmail.com>
2016-07-31 17:44:00 -0700

    Done
    Tried to add the advanced 2 feature with fizzIncrement and
    buzzIncrement. Not sure if I’m doing it correctly. I got the code to
    compile, but still have warning flags.

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -13,6 +13,32 @@
      
      */
     
+    NSUInteger start = 1;
+    NSUInteger limit = 100;
+    
+    NSUInteger *fizzIncrement = 3;
+    NSUInteger *buzzIncrement = 5;
+    int fizzIntIncrement = (int)fizzIncrement;
+    int buzzIntIncrement = (int)buzzIncrement;
+    
+    for (NSUInteger i = start; i <= limit; i++) {
+        NSString *result = @"";
+        
+        if (i % fizzIntIncrement == 0 && i % buzzIntIncrement == 0) {
+            result = @"FizzBuzz";
+        }
+        else if (i % fizzIntIncrement == 0) {    // becomes an 'else if' statement
+            result = @"Fizz";
+        }
+        else if (i % buzzIntIncrement == 0) {
+            result = @"Buzz";
+        }
+        else {
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        NSLog(@"%@", result);
+    }
+    
     // do not alter
     return YES;  //
     ///////////////
